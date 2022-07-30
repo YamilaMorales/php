57b77790e603985fbc1be9e837f803f9e3aa2f25
YamilaMorales <moralesyn@hotmail.com>
2022-07-30 13:39:29 -0300

    cambios

---
+++ formulario_login/index.php
@@ -0,0 +1,30 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+ 
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<main class="container">
+    <div class="row">
+     <div class="col-12 text-center p-5">
+      <h1>Formulario</h1>
+     </div>
+    </div> 
+    
+</main>
+</body>
+</html>
